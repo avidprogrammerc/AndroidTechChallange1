253e9d737328b775b65a165a03313b15a3534005
Chris Conley <cconley@captechventures.com>
2016-08-04 13:13:25 -0400

    Implemented save date/time when app is opened

--- app/src/main/java/com/captechconsulting/techchallange1/MainActivity.java
+++ app/src/main/java/com/captechconsulting/techchallange1/MainActivity.java
@@ -3,9 +3,18 @@ package com.captechconsulting.techchallange1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String FILENAME = "saved_datetimes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateDates() {
+        File file = new File(this.getFilesDir(), FILENAME);
+        FileOutputStream outputStream;
 
+        try {
+            outputStream = new FileOutputStream(file, true);
+
+            DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            String date = formatter.format(new Date()) + "\n";
+
+            outputStream.write(date.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            Log.e("FILE WRITTER", e.getMessage());
+        }
     }
 }
